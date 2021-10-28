9c3004fe347f6cabe8d335721dfde54f7a239b20
facelessmindz <fuzzzzzz@gmail.com>
2021-10-28 06:42:52 +0200

    Add Version for ZBrush 2021

--- batch/upgrade-zSmart-ui.-2021.bat
+++
@@ -1,5 +0,0 @@
-:: Smart UI
-
-:: Upgrade SMART UI plugin in Zbrush 2021\ZStartup\Zplug64 directory
-xcopy /s /y "L:\programming\WORKSPACES\zbrush-smart-ui\Smart_UI_2021.txt" "D:\Imaging\Pixologic\ZBrush 2021\ZStartup\ZPlugs64\" /f
-
