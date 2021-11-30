01e5f43374835a467beaa5f5e9b1adfb603e3caf
facelessmindz <fuzzzzzz@gmail.com>
2021-11-30 16:54:35 +0100

    - adding ZBrush 2022 support - removed year version number for plugin data directory - fix the smart "ui:brush options:a.left" with wrong width value.

---
+++ batch/upgrade-zSmart-ui-2022.bat
@@ -0,0 +1,4 @@
+:: Smart UI
+
+:: Upgrade SMART UI plugin in Zbrush 2021\ZStartup\Zplug64 directory
+xcopy /s /y "L:\programming\WORKSPACES\zbrush-smart-ui\Smart-UI-2022.txt" "D:\Imaging\Pixologic\ZBrush 2022\ZStartup\ZPlugs64\" /f
